4b3ab912162cf81802ac99ab6c8aeb760fd53574
Richard Shields <richard@sorenson.com>
2021-03-30 05:26:51 -0600

    Added useModal. Converted ContextMenu and ExclusiveState to TypeScript.

---
+++ useModal.tsx
@@ -0,0 +1,67 @@
+import React, { useState, ReactElement } from 'react';
+
+type ShowCallback = (() => void);
+type OnSave = (() => void);
+type OnHide = (() => void);
+type OnConfirm = (() => void);
+
+export interface ModalProps {
+  show: boolean,
+  onHide: OnHide,
+  onConfirm: OnConfirm,
+}
+
+function useModal(
+  Dialog: React.FC<ModalProps>,
+  onSave?: OnSave,
+): [() => ReactElement | null, ShowCallback];
+
+function useModal<T>(
+  Dialog: React.FC<T & ModalProps>,
+  onSave?: OnSave,
+): [(props: T) => ReactElement | null, ShowCallback];
+
+function useModal<T>(
+  Dialog: React.FC<T & ModalProps>,
+  onSave?: OnSave,
+): [(props: T) => ReactElement | null, ShowCallback] {
+  const [show, setShow] = useState(false);
+
+  const handleHide = () => {
+    setShow(false);
+  };
+
+  const handleSave = () => {
+    if (onSave) {
+      onSave();
+    }
+
+    handleHide();
+  };
+
+  const createDialog = (props: T): ReactElement | null => {
+    if (show) {
+      const props2 = {
+        ...props,
+        show,
+        onHide: handleHide,
+        onConfirm: handleSave,
+      };
+
+      return (
+        <Dialog
+          {...props2}
+        />
+      );
+    }
+
+    return null;
+  };
+
+  return [
+    createDialog,
+    () => setShow(true),
+  ];
+}
+
+export default useModal;
